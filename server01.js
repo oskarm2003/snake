6152893b6f23b1d5ef6689e3d74a02a6b9aca56a
oskar <oskarm0503@gmail.com>
2022-02-02 22:56:54 +0100

    commit 3

--- server01.js
+++ server01.js
@@ -5,7 +5,7 @@ var path = require("path")
 app.use(express.static('static'))
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + "/snake/index.html"))
+    res.sendFile(path.join(__dirname + "/static/index.html"))
 })
 
 app.listen(PORT, function () {
