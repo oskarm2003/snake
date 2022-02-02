750d160adb2eaa9f91bb07d1c53d7943d66a814a
oskar <oskarm0503@gmail.com>
2022-02-02 22:40:09 +0100

    commit 1

---
+++ server01.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 3000;
+var path = require("path")
+app.use(express.static('static'))
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + "/snake/index.html"))
+})
+
+app.listen(PORT, function () {
+    console.log("start serwera na porcie " + PORT)
+})
