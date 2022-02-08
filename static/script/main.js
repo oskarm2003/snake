e45b90fc1805cf093991df772698b6bdfc930da7
oskar <oskarm0503@gmail.com>
2022-02-08 13:47:31 +0100

    commit6

--- static/script/main.js
+++ static/script/main.js
@@ -5,6 +5,7 @@ const snake = {
     len: 3,
     direction: "right",
     mv: null,
+    queue: "right",
     previousMv: "right",
 }
 
@@ -90,36 +91,8 @@ function eatApple() {
     document.getElementById("apple").style.top = "25%"
     document.getElementById("apple").style.left = "25%"
     setTimeout(() => {
-        // document.getElementById("apple").remove()
         randomApple()
     }, 200)
-}
-
-function mouse() {
-    let pass = 0
-    let x, y
-    let mouse = document.createElement("img")
-    mouse.id = "mouse"
-    mouse.setAttribute("src", "images/mouse.svg")
-    document.getElementById("gameboard").append(mouse)
-    mouseMove = setInterval(() => {
-        while (pass == 0) {
-            x = Math.floor(Math.random() * settings.width) - 1
-            y = Math.floor(Math.random() * settings.height) - 1
-            if (gameArray[x][y] == 0) {
-                pass = 1
-            }
-        }
-        document.getElementById("mouse").style.left = x * 50 + "px"
-        document.getElementById("mouse").style.top = y * 50 + "px"
-        positions[2].x = document.getElementById("mouse").offsetLeft
-        positions[2].y = document.getElementById("mouse").offsetTop
-    }, 2000)
-    setTimeout(() => {
-        clearInterval(mouseMove)
-        document.getElementById("mouse").remove()
-    }, 10000)
-
 }
 
 function start() {
@@ -136,144 +109,161 @@ function start() {
 }
 
 function move(x, y) {
-    snake.mv = setInterval(() => {
-        for (let i = 0; i < settings.height; i++) {
-            for (let j = 0; j < settings.width; j++) {
-                if (gameArray[i][j] > 0) {
-                    gameArray[i][j] += 1
-                }
-                if (gameArray[i][j] > snake.len) {
-                    gameArray[i][j] = 0
-                    document.getElementById(`${i}.${j}`).innerHTML = ""
-                }
-            }
-        }
-        if (snake.direction == "right") {
-            y += 1
-        }
-        if (snake.direction == "left") {
-            y -= 1
-        }
-        if (snake.direction == "up") {
-            x -= 1
-        }
-        if (snake.direction == "down") {
-            x += 1
-        }
-        if (gameArray?.[x]?.[y] > 0 || x < 0 || x > settings.width - 1 || y < 0 || y > settings.height - 1) {
-            clearInterval(snake.mv)
-            alert("koniec :cc")
-            return (0)
-        }
-        if (gameArray[x][y] == -1) {
-            eatApple()
-        }
-        gameArray[x][y] = 1
-        for (let i = 0; i < settings.height; i++) {
-            for (let j = 0; j < settings.width; j++) {
-                if (gameArray[i][j] == 1) {
-                    document.getElementById(`${i}.${j}`).innerHTML += settings.head
-                    document.getElementById("head").style.left = j * 50 + "px"
-                    if (snake.direction == "right") {
-                        document.getElementById("head").style.transform = "rotate(0deg)"
-                    }
-                    if (snake.direction == "left") {
-                        document.getElementById("head").style.transform = "rotate(180deg)"
-                    }
-                    if (snake.direction == "up") {
-                        document.getElementById("head").style.transform = "rotate(-90deg)"
-                    }
-                    if (snake.direction == "down") {
-                        document.getElementById("head").style.transform = "rotate(90deg)"
-                    }
-                }
-                if (gameArray[i][j] == 2) {
-                    document.getElementById(`${i}.${j}`).innerHTML = settings.body
-                    if (snake.previousMv == "right") {
-                        document.getElementById("body").style.transform = "rotate(0deg)"
-                        document.getElementById("body").setAttribute("id", "")
-                    }
-                    if (snake.previousMv == "left") {
-                        document.getElementById("body").style.transform = "rotate(180deg)"
-                        document.getElementById("body").setAttribute("id", "")
-                    }
-                    if (snake.previousMv == "down") {
-                        document.getElementById("body").style.transform = "rotate(90deg)"
-                        document.getElementById("body").setAttribute("id", "")
-                    }
-                    if (snake.previousMv == "up") {
-                        document.getElementById("body").style.transform = "rotate(-90deg)"
-                        document.getElementById("body").setAttribute("id", "")
-                    }
-
-                }
-                if (snake.direction != snake.previousMv && gameArray[i][j] == 2) {
-                    document.getElementById(`${i}.${j}`).innerHTML = settings.turn
-                    if (snake.direction == "right") {
-                        if (snake.previousMv == "down") {
-                            document.getElementById("turn").style.transform = "scaleX(-1)"
-                            // document.getElementById("turn").style.transform += "scaleY(-1)"
-                            document.getElementById("turn").setAttribute("id", "")
-                        }
-                        else if (snake.previousMv == "up") {
-                            document.getElementById("turn").style.transform = "scaleX(-1)"
-                            document.getElementById("turn").style.transform += "scaleY(-1)"
-                            document.getElementById("turn").setAttribute("id", "")
-                        }
-                    }
-                    else if (snake.direction == "left") {
-                        if (snake.previousMv == "down") {
-                            document.getElementById("turn").style.transform = "scaleX(1)"
-                            document.getElementById("turn").setAttribute("id", "")
-                        }
-                        else if (snake.previousMv == "up") {
-                            document.getElementById("turn").style.transform = "scaleY(-1)"
-                            document.getElementById("turn").setAttribute("id", "")
-                        }
-                    }
-                    else if (snake.direction == "up") {
-                        if (snake.previousMv == "right") {
-                            document.getElementById("turn").style.transform = "scaleX(1)"
-                            document.getElementById("turn").setAttribute("id", "")
-                        }
-                        else if (snake.previousMv == "left") {
-                            document.getElementById("turn").style.transform = "scaleX(-1)"
-                            document.getElementById("turn").setAttribute("id", "")
-                        }
-                    }
-                    else if (snake.direction == "down") {
-                        if (snake.previousMv == "right") {
-                            document.getElementById("turn").style.transform = "scaleY(-1)"
-                            document.getElementById("turn").setAttribute("id", "")
-                        }
-                        else if (snake.previousMv == "left") {
-                            document.getElementById("turn").style.transform = "scaleY(-1)"
-                            document.getElementById("turn").style.transform += "scaleX(-1)"
-                            document.getElementById("turn").setAttribute("id", "")
-                        }
-                    }
-                }
-
-                if (gameArray[i][j] == snake.len) {
-                    document.getElementById(`${i}.${j}`).innerHTML = settings.tail
-                    if (gameArray[i][j + 1] == snake.len - 1) {
-                        document.getElementById("tail").style.transform = "rotate(0deg)"
-                    }
-                    if (gameArray[i][j - 1] == snake.len - 1) {
-                        document.getElementById("tail").style.transform = "rotate(180deg)"
-                    }
-                    if (gameArray?.[i + 1]?.[j] == snake.len - 1) {
-                        document.getElementById("tail").style.transform = "rotate(90deg)"
-                    }
-                    if (gameArray?.[i - 1]?.[j] == snake.len - 1) {
-                        document.getElementById("tail").style.transform = "rotate(-90deg)"
-                    }
-                }
-            }
-        }
-        snake.previousMv = snake.direction
-        // render()
+    setInterval(() => {
+        snake.direction = snake.queue
     }, 100)
+    setTimeout(() => {
+        snake.mv = setInterval(() => {
+            for (let i = 0; i < settings.height; i++) {
+                for (let j = 0; j < settings.width; j++) {
+                    if (gameArray[i][j] > 0) {
+                        gameArray[i][j] += 1
+                    }
+                }
+            }
+            if (snake.direction == "right") {
+                y += 1
+            }
+            if (snake.direction == "left") {
+                y -= 1
+            }
+            if (snake.direction == "up") {
+                x -= 1
+            }
+            if (snake.direction == "down") {
+                x += 1
+            }
+            if (gameArray?.[x]?.[y] > 0 || x < 0 || x > settings.width - 1 || y < 0 || y > settings.height - 1) {
+                clearInterval(snake.mv)
+                let msg = document.createElement("p")
+                msg.id = "msg"
+                msg.innerHTML = `The end :(<br/>At least the length of your snake was ${snake.len}. Make it bigger!<p id="reset">Press any key to try again...</p>`
+                document.getElementById("gameboard").append(msg)
+                setTimeout(() => {
+                    document.addEventListener('keydown', logKey);
+                    function logKey(e) {
+                        window.location.reload()
+                    }
+                }, 1000)
+                return (0)
+            }
+            for (let i = 0; i < settings.height; i++) {
+                for (let j = 0; j < settings.width; j++) {
+                    if (gameArray[i][j] > snake.len) {
+                        gameArray[i][j] = 0
+                        document.getElementById(`${i}.${j}`).innerHTML = ""
+                    }
+                }
+            }
+            if (gameArray[x][y] == -1) {
+                eatApple()
+            }
+            gameArray[x][y] = 1
+            for (let i = 0; i < settings.height; i++) {
+                for (let j = 0; j < settings.width; j++) {
+                    if (gameArray[i][j] == 1) {
+                        document.getElementById(`${i}.${j}`).innerHTML += settings.head
+                        document.getElementById("head").style.left = j * 50 + "px"
+                        if (snake.direction == "right") {
+                            document.getElementById("head").style.transform = "rotate(0deg)"
+                        }
+                        if (snake.direction == "left") {
+                            document.getElementById("head").style.transform = "rotate(180deg)"
+                        }
+                        if (snake.direction == "up") {
+                            document.getElementById("head").style.transform = "rotate(-90deg)"
+                        }
+                        if (snake.direction == "down") {
+                            document.getElementById("head").style.transform = "rotate(90deg)"
+                        }
+                    }
+                    if (gameArray[i][j] == 2) {
+                        document.getElementById(`${i}.${j}`).innerHTML = settings.body
+                        if (snake.previousMv == "right") {
+                            document.getElementById("body").style.transform = "rotate(0deg)"
+                            document.getElementById("body").setAttribute("id", "")
+                        }
+                        if (snake.previousMv == "left") {
+                            document.getElementById("body").style.transform = "rotate(180deg)"
+                            document.getElementById("body").setAttribute("id", "")
+                        }
+                        if (snake.previousMv == "down") {
+                            document.getElementById("body").style.transform = "rotate(90deg)"
+                            document.getElementById("body").setAttribute("id", "")
+                        }
+                        if (snake.previousMv == "up") {
+                            document.getElementById("body").style.transform = "rotate(-90deg)"
+                            document.getElementById("body").setAttribute("id", "")
+                        }
+
+                    }
+                    if (snake.direction != snake.previousMv && gameArray[i][j] == 2) {
+                        document.getElementById(`${i}.${j}`).innerHTML = settings.turn
+                        if (snake.direction == "right") {
+                            if (snake.previousMv == "down") {
+                                document.getElementById("turn").style.transform = "scaleX(-1)"
+                                // document.getElementById("turn").style.transform += "scaleY(-1)"
+                                document.getElementById("turn").setAttribute("id", "")
+                            }
+                            else if (snake.previousMv == "up") {
+                                document.getElementById("turn").style.transform = "scaleX(-1)"
+                                document.getElementById("turn").style.transform += "scaleY(-1)"
+                                document.getElementById("turn").setAttribute("id", "")
+                            }
+                        }
+                        else if (snake.direction == "left") {
+                            if (snake.previousMv == "down") {
+                                document.getElementById("turn").style.transform = "scaleX(1)"
+                                document.getElementById("turn").setAttribute("id", "")
+                            }
+                            else if (snake.previousMv == "up") {
+                                document.getElementById("turn").style.transform = "scaleY(-1)"
+                                document.getElementById("turn").setAttribute("id", "")
+                            }
+                        }
+                        else if (snake.direction == "up") {
+                            if (snake.previousMv == "right") {
+                                document.getElementById("turn").style.transform = "scaleX(1)"
+                                document.getElementById("turn").setAttribute("id", "")
+                            }
+                            else if (snake.previousMv == "left") {
+                                document.getElementById("turn").style.transform = "scaleX(-1)"
+                                document.getElementById("turn").setAttribute("id", "")
+                            }
+                        }
+                        else if (snake.direction == "down") {
+                            if (snake.previousMv == "right") {
+                                document.getElementById("turn").style.transform = "scaleY(-1)"
+                                document.getElementById("turn").setAttribute("id", "")
+                            }
+                            else if (snake.previousMv == "left") {
+                                document.getElementById("turn").style.transform = "scaleY(-1)"
+                                document.getElementById("turn").style.transform += "scaleX(-1)"
+                                document.getElementById("turn").setAttribute("id", "")
+                            }
+                        }
+                    }
+
+                    if (gameArray[i][j] == snake.len) {
+                        document.getElementById(`${i}.${j}`).innerHTML = settings.tail
+                        if (gameArray[i][j + 1] == snake.len - 1) {
+                            document.getElementById("tail").style.transform = "rotate(0deg)"
+                        }
+                        if (gameArray[i][j - 1] == snake.len - 1) {
+                            document.getElementById("tail").style.transform = "rotate(180deg)"
+                        }
+                        if (gameArray?.[i + 1]?.[j] == snake.len - 1) {
+                            document.getElementById("tail").style.transform = "rotate(90deg)"
+                        }
+                        if (gameArray?.[i - 1]?.[j] == snake.len - 1) {
+                            document.getElementById("tail").style.transform = "rotate(-90deg)"
+                        }
+                    }
+                }
+            }
+            snake.previousMv = snake.direction
+        }, 100)
+    }, 10)
 }
 
 function render() {
@@ -292,16 +282,16 @@ function logKey(e) {
         first = 0
     }
     if (e.which == "39" && snake.direction != "left" && snake.direction != "right" || e.which == "68" && snake.direction != "left" && snake.direction != "right") {
-        snake.direction = "right"
+        snake.queue = "right"
     }
     else if (e.which == "37" && snake.direction != "right" && snake.direction != "left" || e.which == "65" && snake.direction != "right" && snake.direction != "left") {
-        snake.direction = "left"
+        snake.queue = "left"
     }
     else if (e.which == "38" && snake.direction != "down" && snake.direction != "up" || e.which == "87" && snake.direction != "down" && snake.direction != "up") {
-        snake.direction = "up"
+        snake.queue = "up"
     }
     else if (e.which == "40" && snake.direction != "up" && snake.direction != "down" || e.which == "83" && snake.direction != "up" && snake.direction != "down") {
-        snake.direction = "down"
+        snake.queue = "down"
     }
 }
 
